ee392b2c56654cdd4b23c3e67f7a5db243a0fad0
rushi1222 <k.rushi1222@gmail.com>
2022-04-12 11:16:33 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
-
-function App() {
+import {Routes,Route,NavLink} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import Home from './Components/Home';
+import GenereicNotFound from './Components/GenereicNotFound';
+import { Navigate } from 'react-router-dom';
+ function App() {
   return (
-    <div className="App">
-     hi
-    </div>
+    <BrowserRouter>
+
+    <Routes>
+ 
+      <Route path = "/" element = {<Home/>}></Route>
+      <Route path = "/GenereicNotFound" element = {<GenereicNotFound/>}></Route>
+      <Route path="*" element={<Navigate to="/GenereicNotFound" replace />} />
+     
+    
+    </Routes>
+    </BrowserRouter>
+
+    
   );
 }
 
---
+++ src/Components/GenereicNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GenereicNotFound() {
+  return (
+    <div>GenereicNotFound</div>
+  )
+}
+
+export default GenereicNotFound
