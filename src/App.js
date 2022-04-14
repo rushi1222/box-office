3d1db5f139b4ed58b0ea3282559a748c801c02ee
rushi1222 <k.rushi1222@gmail.com>
2022-04-14 14:58:40 +0530

    Created navabr

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import {Routes,Route,NavLink} from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom';
-import Home from './Components/Home';
 import GenereicNotFound from './Components/GenereicNotFound';
 import { Navigate } from 'react-router-dom';
+import Nav from './Components/Nav';
+import Home from './Pages/Home';
+import Starred from './Pages/Starred';
  function App() {
   return (
     <BrowserRouter>
 
+    <div>
+
+    <Nav/>
     <Routes>
  
-      <Route path = "/" element = {<Home/>}></Route>
+      <Route  path = "/" element = {<Home/>}></Route>
+      <Route  path = "/Starred" element = {<Starred/>}></Route>
       <Route path = "/GenereicNotFound" element = {<GenereicNotFound/>}></Route>
       <Route path="*" element={<Navigate to="/GenereicNotFound" replace />} />
      
     
     </Routes>
+    </div>
     </BrowserRouter>
 
     
