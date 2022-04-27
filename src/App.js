445f63b20ee2e6403b8267cab77dcffa07e821cc
rushi1222 <k.rushi1222@gmail.com>
2022-04-27 16:02:07 +0530

    reducer and a few more

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Navigate } from 'react-router-dom';
 import Navs from './components/Navs';
 import Home from './Pages/Home';
 import Starred from './Pages/Starred';
+import Show from './Pages/Show';
  function App() {
   return (
   
@@ -17,6 +18,7 @@ import Starred from './Pages/Starred';
       <Route  path = "/" element = {<Home/>}></Route>
       <Route  path = "/Starred" element = {<Starred/>}></Route>
       <Route path = "/GenericNotFound" element = {<GenericNotFound/>}></Route>
+      <Route path = "/Show/:id" element = {<Show/>}></Route>
       <Route path="*" element={<Navigate to="/GenereicNotFound" replace />} />
      
     
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 import { apiGet } from '../misc/config';
 import ShowGrid from '../components/show/ShowGrid';
@@ -10,7 +10,15 @@ const Home = () => {
   const [searchOption, setSearchOption] = useState('shows');
 
   const isShowsSearch = searchOption === 'shows';
+useEffect (()=>{
+  console.log('use effectt run')
 
+  return ()=> {
+    console.log('exit');
+  }
+},[]); 
+
+  
   const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
