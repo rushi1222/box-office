74b3e9becffcd4a21ac68177f3d894d9c7055ec5
rushi1222 <k.rushi1222@gmail.com>
2022-04-28 20:35:13 +0530

    styling at last

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ import Navs from './components/Navs';
 import Home from './Pages/Home';
 import Starred from './Pages/Starred';
 import Show from './Pages/Show';
+import { ThemeProvider } from 'styled-components';
  function App() {
+
+  const theme = {
+    mainColors: {
+      blue: '#2400ff',
+      gray: '#c6c6c6',
+      dark: '#353535',
+    },
+  };
   return (
   
 
     <div>
 
-    
+<ThemeProvider theme={theme}>
     <Routes>
  
       <Route  path = "/" element = {<Home/>}></Route>
@@ -23,6 +32,8 @@ import Show from './Pages/Show';
      
     
     </Routes>
+    </ThemeProvider>
+
     </div>
     
 
